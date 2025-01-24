70808233d38685558632b95e4887991e02df2cc6
Null <pairmanxlr@gmail.com>
2025-01-24 09:57:38 +0800

    fix inventories for armorstands, fixes #1017

--- src/main/java/carpet/script/value/NBTSerializableValue.java
+++ src/main/java/carpet/script/value/NBTSerializableValue.java
@@ -233,11 +233,11 @@ public class NBTSerializableValue extends Value implements ContainerValueInterfa
             {
                 Inventory inv = null;
                 Entity e = ((EntityValue) v1).getEntity();
-                if (e instanceof PlayerEntity) inv = ((PlayerEntity) e).inventory;
+                if (e instanceof PlayerEntity pe) inv = pe.inventory;
                 else if (e instanceof Inventory) inv = (Inventory) e;
-                else if (e instanceof VillagerEntity) inv = ((VillagerEntity) e).getInventory();
-                else if (e instanceof InventoryBearerInterface) inv = ((InventoryBearerInterface)e).getCMInventory();
-                else if (e instanceof LivingEntity) return new InventoryLocator(e, e.getBlockPos(), new EquipmentInventory((MobEntity) e), offset+1);
+                else if (e instanceof VillagerEntity ve) inv = ve.getInventory();
+                else if (e instanceof InventoryBearerInterface ibi) inv = ibi.getCMInventory();
+                else if (e instanceof LivingEntity le) return new InventoryLocator(e, e.getBlockPos(), new EquipmentInventory(le, offset+1);
                 if (inv == null)
                     return null;
 
