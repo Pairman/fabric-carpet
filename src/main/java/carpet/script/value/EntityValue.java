82b0f296fd297c5676ca7220ff62b3a91afc6072
Pairman Guo <pairmanxlr@gmail.com>
2025-01-25 09:24:25 +0800

    Fixes #1222 (#1224)

--- src/main/java/carpet/script/value/EntityValue.java
+++ src/main/java/carpet/script/value/EntityValue.java
@@ -1461,7 +1461,7 @@ public class EntityValue extends Value
         });
 
         put("absorption", (e, v) -> {
-            if (e instanceof PlayerEntity) ((PlayerEntity) e).setAbsorptionAmount((float) NumericValue.asNumber(v, "absorbtion").getLong());
+            if (e instanceof PlayerEntity) ((PlayerEntity) e).setAbsorptionAmount(NumericValue.asNumber(v, "absorbtion").getFloat());
         });
 
         put("add_xp", (e, v) -> {
